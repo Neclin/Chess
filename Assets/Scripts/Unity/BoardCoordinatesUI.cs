52ce891cd68b0daa98fcc5968d6c0ec306fdc13e
Neclin <chapmanluke05@icloud.com>
2026-04-30 11:22:49 +0100

    drag-drop, color palette, scene wiring with TextMesh Pro essentials, fix king/queen sprite names

--- Assets/Scripts/Unity/BoardCoordinatesUI.cs
+++ Assets/Scripts/Unity/BoardCoordinatesUI.cs
@@ -3,14 +3,27 @@ using TMPro;
 
 namespace Chess.Unity
 {
+    [ExecuteAlways]
     public sealed class BoardCoordinatesUI : MonoBehaviour
     {
         public BoardView Board;
         public GameObject LabelPrefab;
         public Transform Container;
+        public bool ShowCoordinates = true;
+
+        private bool _hasSpawnedLabels;
 
         private void Start()
         {
+            if (Application.isPlaying) SpawnLabelsIfNeeded();
+            ApplyVisibility();
+        }
+
+        private void OnValidate() => ApplyVisibility();
+
+        private void SpawnLabelsIfNeeded()
+        {
+            if (_hasSpawnedLabels || Container == null || LabelPrefab == null) return;
             for (int fileIndex = 0; fileIndex < 8; fileIndex++)
             {
                 string fileLabel = ((char)('a' + fileIndex)).ToString();
@@ -23,6 +36,7 @@ namespace Chess.Unity
                 Spawn(rankLabel, new Vector3(-4.2f, rankIndex - 3.5f, 0f));
                 Spawn(rankLabel, new Vector3(4.2f, rankIndex - 3.5f, 0f));
             }
+            _hasSpawnedLabels = true;
         }
 
         private void Spawn(string labelText, Vector3 localPosition)
@@ -30,6 +44,11 @@ namespace Chess.Unity
             var labelGameObject = Instantiate(LabelPrefab, Container);
             labelGameObject.transform.localPosition = localPosition;
             labelGameObject.GetComponent<TMP_Text>().text = labelText;
+        }
+
+        private void ApplyVisibility()
+        {
+            if (Container != null) Container.gameObject.SetActive(ShowCoordinates);
         }
     }
 }
--- Assets/Scripts/Unity/SquareView.cs
+++ Assets/Scripts/Unity/SquareView.cs
@@ -3,11 +3,46 @@ using UnityEngine.EventSystems;
 
 namespace Chess.Unity
 {
-    public sealed class SquareView : MonoBehaviour, IPointerClickHandler
+    [RequireComponent(typeof(SpriteRenderer))]
+    public sealed class SquareView : MonoBehaviour,
+        IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
         public int SquareIndex;
+        public bool IsLightSquare;
+        public BoardColorPalette ColorPalette;
+
         public System.Action<int> OnClick;
+        public System.Action<int> OnDragBegin;
+        public System.Action<int, Vector3> OnDragMove;
+        public System.Action<int, Vector3> OnDragEnd;
+
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake() => _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        public void SetHighlightState(SquareHighlightState highlightState)
+        {
+            if (_spriteRenderer == null) _spriteRenderer = GetComponent<SpriteRenderer>();
+            if (ColorPalette == null) return;
+            _spriteRenderer.color = ColorPalette.GetColor(IsLightSquare, highlightState);
+        }
 
         public void OnPointerClick(PointerEventData pointerEventData) => OnClick?.Invoke(SquareIndex);
+
+        public void OnBeginDrag(PointerEventData pointerEventData) => OnDragBegin?.Invoke(SquareIndex);
+
+        public void OnDrag(PointerEventData pointerEventData)
+            => OnDragMove?.Invoke(SquareIndex, ScreenPositionToWorldPosition(pointerEventData.position));
+
+        public void OnEndDrag(PointerEventData pointerEventData)
+            => OnDragEnd?.Invoke(SquareIndex, ScreenPositionToWorldPosition(pointerEventData.position));
+
+        private static Vector3 ScreenPositionToWorldPosition(Vector2 screenPosition)
+        {
+            var mainCamera = Camera.main;
+            if (mainCamera == null) return Vector3.zero;
+            float depthFromCamera = -mainCamera.transform.position.z;
+            return mainCamera.ScreenToWorldPoint(new Vector3(screenPosition.x, screenPosition.y, depthFromCamera));
+        }
     }
 }
